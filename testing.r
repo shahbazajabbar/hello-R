6bbcf4d70dd9c727a2868ac89bea8e636539eb3d
shahbazajabbar <shaahbazajabbar@yahoo.com>
2020-10-12 18:10:28 +0500

    New lines added on 12-10-2020

--- testing.r
+++ testing.r
@@ -1,3 +1,15 @@
 # This is from Github
 print ("Hello from Github")
 
+name = "Muhammad Shahbaz"
+version = "Rstudio"
+
+print (name)
+print ("These lines are written in RStudio")
+
+
+name = "Pakistan"
+print (name)
+
+province = "Punjab"
+print (province)
